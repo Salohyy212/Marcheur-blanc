bd9c15dbccf992cdfac365773992dd343d09b449
Salohyy212 <zerambaysalohy@gmail.com>
2024-06-17 18:30:29 +0300

    chore: add constructor parameter

--- src/main/java/org/salohy/Rue.java
+++ src/main/java/org/salohy/Rue.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 
 @Getter
 public record Rue(Lieu lieu1, Lieu lieu2, String nom) {
+    public Rue(Lieu lieu1, Lieu lieu2, String nom) {
+        this.lieu1 = lieu1;
+        this.lieu2 = lieu2;
+        this.nom = nom;
+    }
     public Lieu getAutreLieu(Lieu lieu){
         if(lieu.equals(lieu1)){
             return lieu2;
