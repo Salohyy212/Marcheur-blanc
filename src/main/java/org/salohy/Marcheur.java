634aad89e7a7a51dde66957d158525d81c05f3c2
Salohyy212 <zerambaysalohy@gmail.com>
2024-06-17 18:38:41 +0300

    chore: change indexAleatoire to indexLieuAdjacent

--- src/main/java/org/salohy/Marcheur.java
+++ src/main/java/org/salohy/Marcheur.java
@@ -1,31 +1,31 @@
 package org.salohy;
 
 import lombok.Getter;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
 @Getter
-public record Marcheur(String nom ) {
-    public Marche marcher(Lieu lieuDepart, Lieu lieuArrivee, Carte carte){
+public record Marcheur(String nom) {
+
+    public Marche marcher(Lieu lieuDepart, Lieu lieuArrivee, Carte carte) {
         List<Lieu> trajet = new ArrayList<>();
         Random random = new Random();
 
         trajet.add(lieuDepart);
         Lieu positionActuelle = lieuDepart;
-        while(!positionActuelle.equals(lieuArrivee)){
-            List<Lieu> lieuxAdjacents = positionActuelle.getLieux();
-            if(lieuxAdjacents.isEmpty()){
+
+        while (!positionActuelle.equals(lieuArrivee)) {
+            List<Lieu> lieuxAdjacents = positionActuelle.getLieuxAdjacents();
+            if (lieuxAdjacents.isEmpty()) {
                 break;
             }
-            int indexAleatoire = random.nextInt((lieuxAdjacents.size()));
-            positionActuelle = lieuxAdjacents.get(indexAleatoire);
+            int indexAdjacent = random.nextInt(lieuxAdjacents.size());
+            positionActuelle = lieuxAdjacents.get(indexAdjacent);
 
             trajet.add(positionActuelle);
-       }
+        }
+
         return new Marche(trajet);
     }
-
 }
