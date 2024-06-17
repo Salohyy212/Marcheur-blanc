5048fd337b5d16c1fadb36bf6c9bdc71426d3244
Salohyy212 <zerambaysalohy@gmail.com>
2024-06-17 18:29:12 +0300

    refactor: change record Carte to class Carte

--- src/main/java/org/salohy/Carte.java
+++ src/main/java/org/salohy/Carte.java
@@ -1,9 +1,29 @@
 package org.salohy;
 
 import lombok.Getter;
-
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
-public record Carte(String nom, Set<Lieu> lieux) {
+public class Carte {
+    private final String nom;
+    private final Set<Lieu> lieux;
+    private final Set<Rue> rues;
+
+    public Carte(String nom) {
+        this.nom = nom;
+        this.lieux = new HashSet<>();
+        this.rues = new HashSet<>();
+    }
+
+    public void ajouterLieu(Lieu lieu) {
+        lieux.add(lieu);
+    }
+
+    public void ajouterRue(Rue rue) {
+        rues.add(rue);
+        rue.getLieu1().ajouterRue(rue);
+        rue.getLieu2().ajouterRue(rue);
+    }
 }
+
