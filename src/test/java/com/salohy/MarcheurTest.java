cdee60898da72802845e1cb7812fc14a01e6c6c1
Salohyy212 <zerambaysalohy@gmail.com>
2024-06-19 09:21:57 +0300

    feat: arrange test MarcheurTest

--- src/test/java/com/salohy/MarcheurTest.java
+++ src/test/java/com/salohy/MarcheurTest.java
@@ -13,7 +13,6 @@ class MarcheurTest {
 
     @Test
     void bjarni_marche_aleatoire_de_HEI_vers_ESTI() {
-        // creation de lieu
         var marais = new Lieu("Marais");
         var sekolintsika = new Lieu("Sekolintsika");
         var hei = new Lieu("HEI");
@@ -23,23 +22,43 @@ class MarcheurTest {
         var nexta = new Lieu("Nexta");
         var esti = new Lieu("ESTI");
 
-        // creation de rue
-        var rue1 = new Rue(marais, sekolintsika,null);
-        var rue2 = new Rue(sekolintsika, hei, null);
+        var rue1 = new Rue(marais, sekolintsika,"");
+        var rue2 = new Rue(sekolintsika, hei, "");
         var rue3 = new Rue(hei, pullman ,"Rue Andriantsihoarana");
-        var rue4 = new Rue(pullman, nexta, null);
+        var rue4 = new Rue(pullman, nexta, "");
         var rue5 = new Rue( pullman, balancoire, "Rue Ranaivo");
-        var rue6 = new Rue(hei, balancoire, null);
-        var rue7 = new Rue(balancoire, boulevardDeLEurope, null);
-        var rue8 = new Rue(balancoire, esti, null);
-        var rue9 = new Rue(boulevardDeLEurope, esti, null);
+        var rue6 = new Rue(hei, balancoire, "");
+        var rue7 = new Rue(balancoire, boulevardDeLEurope, "");
+        var rue8 = new Rue(balancoire, esti, "");
+        var rue9 = new Rue(boulevardDeLEurope, esti, "");
+
+        marais.ajouterRue(rue1);
+
+        sekolintsika.ajouterRue(rue1);
+        sekolintsika.ajouterRue((rue2));
+
+        hei.ajouterRue(rue2);
+        hei.ajouterRue(rue3);
+        hei.ajouterRue(rue6);
+
+        pullman.ajouterRue(rue3);
+        pullman.ajouterRue(rue4);
+        pullman.ajouterRue(rue5);
+
+        balancoire.ajouterRue(rue6);
+        balancoire.ajouterRue(rue7);
+        balancoire.ajouterRue(rue8);
+
+        boulevardDeLEurope.ajouterRue(rue7);
+        boulevardDeLEurope.ajouterRue(rue9);
+
+        esti.ajouterRue(rue8);
+        esti.ajouterRue(rue9);
 
         var carteTana = new Carte(Set.of(rue1, rue2, rue3, rue4, rue5, rue6, rue7, rue8, rue9).toString());
 
-        // creation d'un marcheur
         var bjarni = new Marcheur("Bjarni");
 
-        // Bjarni marche de Hei vers Esti
         Marche trajet = bjarni.marcher(hei, esti);
 
         var lieuxVisites = trajet.lieuxVisites();
@@ -48,6 +67,10 @@ class MarcheurTest {
 
         assertEquals(hei, lieuxVisites.getFirst(), "La marche commence à HEI");
         assertEquals(esti, lieuxVisites.getLast(), "La marche se termine à ESTI");
+
+        for (int i = 0; i < lieuxVisites.size(); i++) {
+            System.out.println(lieuxVisites.get(i).getNom());
+        }
 
         int deplacementMinimale = 3;
 
